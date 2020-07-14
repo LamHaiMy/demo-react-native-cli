a13b004e2ce160c2fbfefbdaa15c1196659fae2e
My Lam <my.lam@silkwires.com>
2020-07-14 11:27:14 +0700

    Div reducer

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ import {
   Dimensions,
   TouchableOpacity,
 } from 'react-native';
-import { createStore } from 'redux';
+import store from './source/Redux/store';
 import { Provider } from 'react-redux';
 import ListItems from './source/Views/ListItems'
 import { TabView, SceneMap } from 'react-native-tab-view';
@@ -74,79 +74,5 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-const defaulState = {
-  listItem: [
-    {
-      id: 1,
-      imageUrl: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 2,
-      imageUrl: 'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 3,
-      imageUrl: 'https://homepages.cae.wisc.edu/~ece533/images/boat.png',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 4,
-      imageUrl: 'https://www.gstatic.com/webp/gallery/1.jpg',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 5,
-      imageUrl: 'https://homepages.cae.wisc.edu/~ece533/images/pool.png',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 6,
-      imageUrl: 'https://www.gstatic.com/webp/gallery/5.jpg',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 7,
-      imageUrl: 'https://www.gstatic.com/webp/gallery/4.jpg',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 8,
-      imageUrl: 'https://www.gstatic.com/webp/gallery/1.jpg',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-    {
-      id: 9,
-      imageUrl: 'https://www.gstatic.com/webp/gallery/3.jpg',
-      productName: 'ABC product',
-      subTitle: 'Limited sub',
-      detailLink: 'link detail'
-    },
-  ],
-};
-const reducer = (state = defaulState, action) => {
-  // if(action.type === CHANGE) return {value: state.value + 1}
-  return state;
-};
 
-const store = createStore(reducer);
-
-const myState = store.getState();
 export default App;
